fd6c314b4af476f4e82f3ca6123a451c898a61f4
Andrew Kyllo <kyllo34@gmail.com>
2020-03-18 17:19:52 -0700

    initial setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+import { enableFetchMocks } from 'jest-fetch-mock'
+
+import { configure } from 'enzyme'
+import 'jest-enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+enableFetchMocks()
